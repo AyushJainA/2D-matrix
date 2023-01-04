c036bd3baeed0d3f1b9814572acca9ea8cd03a24
Ayush Jain <116271677+AyushJainA@users.noreply.github.com>
2023-01-04 19:41:36 +0530

    Add files via upload

---
+++ 2-D array in dynamic memory.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+
+	int row ,col;
+	cin>>row>>col;
+	
+	int *arr=  new int [col];
+	
+	//creating a 2-d array dynamically 
+	int **array= new int *[row];
+	for(int i =0;i<row;i++){
+		array[i]=new int[col]; 
+	}
+
+	//taking input 
+	for(int i=0;i<row;i++){
+	for(int j=0;j<col;j++) { cin>>array[i][j];
+	}	}
+	
+	//Output
+		for(int i=0;i<row;i++){
+	for(int j=0;j<col;j++) { cout<<array[i][j]<<" ";
+	}cout<<endl;	}
+	
+	//releasing memory 
+		for(int i =0;i<row;i++){
+		delete []array[i];
+	}
+
+	delete[]array;
+
+
+
+
+return 0;
+}
+
+
